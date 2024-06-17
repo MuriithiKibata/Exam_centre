cfd543a44810e5e1b194cc0a477e31a54f1e9153
MuriithiKibata <kibatamuriithi@gmail.com>
2024-06-18 00:16:03 +0300

    🚑️ Fix
    adds missing dependencies

--- app.py
+++ app.py
@@ -1,31 +1,3 @@
-# from database.setup import create_tables
-# from models.candidate import Candidate
-# from models.invigilator import Invigilator
-# from models.subject import Subject
-
-
-
-# def initialize():
-#     print("creating tables ...")
-#     create_tables()
-#     print("Tables created successfully")
-
-
-# def display_menu():
-#     print("1. Add Candidate:")
-#     print("2. Add Subject")
-#     print("3. Add invigillator")
-#     print("3. Display Candidate")
-#     print("4. Display Subject")
-#     print("5. ")
-
-# def main():
-#     initialize()
-#     display_menu()
-
-# if __name__ == "__main__":
-#     main()
-
 from database.setup import create_tables
 from models.candidate import Candidate
 from models.invigilator import Invigilator
@@ -40,7 +12,7 @@ def add_candidate():
     name = input("Enter candidate name: ")
     national_id = input("Enter candidate national ID: ")
     e_mail = input("Enter candidate email: ")
-    
+
     # Display available subjects
     subjects = Subject.get_all()
     print("Available Subjects:")
